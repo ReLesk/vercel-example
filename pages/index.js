1662cd045f37e380d0847f01b86820bbc0a7995c
reLesk <outdaywork@gmail.com>
2021-09-18 19:04:38 -0400

    home module fixed

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '/components/layout'
 
-import '/styles/home.module.css'
-
 export default function Home() {
   return (
     <Layout>
